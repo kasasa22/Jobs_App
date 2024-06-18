81d6a951fe4facedbdeb59386cf7ffce87de6099
Trevor22 <kasasatrevor25@gmail.com>
2024-06-18 23:15:19 +0300

    index

--- app/index.js
+++ app/index.js
@@ -8,17 +8,33 @@ import { Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome } from '../components
 const Home = () => {
     const router = useRouter();
     return (
-        <SafeAreaView style={{ flex:1, background:COLORS.
-            lightWhite
-         }}>
-            
-           <Stack.Screen
-           options={{ 
-            headerStyle:{
-                backgroundColor: COLORS.lightWhite
-            }
-            }},
-          <SafeAreaView/>
+        <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+            <Stack.Screen
+                options={{ 
+                    headerStyle: {
+                        backgroundColor: COLORS.lightWhite
+                    },
+                    headerShadowVisible: false,
+                    headerLeft: () => (
+                        <ScreenHeaderBtn 
+                            iconUrl={icons.menu} dimensions="100%"
+                        />
+                    ),
+                    headerRight: () => (
+                        <ScreenHeaderBtn 
+                            iconUrl={icons.profile} dimensions="100%"
+                        />
+                    )
+                }}
+            />
+            {/* Add your ScrollView and other components here */}
+            <ScrollView>
+                <View>
+                    <Text>Welcome to the Home Screen</Text>
+                    {/* Include other components like Nearbyjobs, Popularjobs, etc. */}
+                </View>
+            </ScrollView>
+        </SafeAreaView>
     );
 }
 

02a22755ee9ed4633291e3913a616a3f08cc3fb4
Trevor22 <kasasatrevor25@gmail.com>
2024-06-19 23:04:09 +0300

    fetching

---
+++ hook/useFetch.js
@@ -0,0 +1,14 @@
+import { Axios } from "axios";
+import { useState,useEffect } from "react";
+import axios from "axios";
+
+import React from 'react'
+
+const useFetch = () => {
+    const [data, setData]=useState([]);
+    const [isLoading, setIsLoading] =useState(false);
+    const [error, setError] = useState(null);
+  
+}
+
+export default useFetch
